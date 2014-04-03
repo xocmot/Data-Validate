244684a2a797b1217ceef7100727f1f4d4153a7e
xocmot <technical@thomasdcox.com>
2014-04-02 20:01:34 -0500

    Create Data-Validate-JQuery.js
    The JQuery JavaScript File that calls the dataValidateAddEventsJQ() Init Method when the document finishes loading...

---
+++ Data-Validate-JQuery.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    dataValidateAddEventsJQ();
+    //**Any JQuery code/events need to go after the dataValidateAddEventsJQ() method above. 
+    //**All events will be executed after the Data-Validate methods have been called, and will be halted in the JQuery DOM if the validation fails.
+    //------>>>[JQUERY CODE GOES HERE]<<<------
+});
